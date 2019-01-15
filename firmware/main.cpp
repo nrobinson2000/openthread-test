1a07fc4ce38b6bcb42e7a8cf6bbcdd2f55da9ba8
Nathan Robinson <nrobinson2000@me.com>
2019-01-15 13:27:04 -0500

    Jeremy Initial

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,11 +1,49 @@
 #include "Particle.h"
 
-void setup() // Put setup code here to run once
-{
+#include "ot_api.h"
+extern "C" {
 
-}
+    int8_t    otLinkGetChannel(otInstance *aInstance);
+    uint16_t  otLinkGetPanId(otInstance *aInstance);     // type otPanId
+    uint8_t   otThreadGetMasterKey(otInstance *aInstance);
+    char     *otThreadGetNetworkName(otInstance *aInstance);
 
-void loop() // Put code here to loop forever
-{
+};
 
+
+void loop() {
+
+   int8_t myChannel = otLinkGetChannel(0);
+   uint16_t myPanID = otLinkGetPanId(0);    // otPanId
+
+   char myPanIDBuff[255];
+   snprintf(myPanIDBuff, sizeof(myPanIDBuff), "0x%X", myPanID);   // put hex into myPanIDBuff
+
+   uint8_t myMasterKey = otThreadGetMasterKey(0);
+   char *myNetworkName = otThreadGetNetworkName(0);
+
+
+   char myNetworkNameBuff[255];
+   snprintf(myNetworkNameBuff, sizeof(myNetworkNameBuff), "%s", myNetworkName);   // network name as a string
+
+
+   Particle.publish("----------------","-------------", 60, PRIVATE);    // just to show a space between samples
+   delay(2000);
+   Particle.publish("Particle Thread ","Connection Information", 60, PRIVATE);    // just to show a space between samples
+   delay(2000);
+   Particle.publish("My Channel: ", String(myChannel), 60, PRIVATE);  //shows printing an integer variable
+
+   delay(2000);
+   Particle.publish("Argon Pan ID: ", String(myPanIDBuff), 60, PRIVATE);  //shows printing an integer variable
+   delay(2000);
+
+   Particle.publish("My Master Key: ", String(myMasterKey), 60, PRIVATE);  //shows printing an integer variable
+   delay(2000);
+   Particle.publish("My Network Name: ", String(myNetworkNameBuff), 60, PRIVATE);  //shows printing an integer variable
+   delay(2000);
+   Particle.publish("----------------","-------------", 60, PRIVATE);    // just to show a space between samples
+   delay(2000);
+   Particle.publish("Generates the same info","every 60 seconds", 60, PRIVATE);    // just to show a space between samples
+
+   delay(60000);  // wait 60 seconds before re-print
 }
