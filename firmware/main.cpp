31d54692f9e9a8cd3fb119c213974af7ecabe015
Nathan Robinson <nrobinson2000@me.com>
2019-01-16 01:00:25 -0500

    Next attempt

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,49 +1,97 @@
 #include "Particle.h"
 
+#define OT_MASTER_KEY_SIZE 16
+
 #include "ot_api.h"
+#include "openthread/dataset.h"
+
+
 extern "C" {
 
-    int8_t    otLinkGetChannel(otInstance *aInstance);
-    uint16_t  otLinkGetPanId(otInstance *aInstance);     // type otPanId
-    uint8_t   otThreadGetMasterKey(otInstance *aInstance);
-    char     *otThreadGetNetworkName(otInstance *aInstance);
+int8_t    otLinkGetChannel(otInstance *aInstance);
+uint16_t  otLinkGetPanId(otInstance *aInstance);         // type otPanId
+otMasterKey otThreadGetMasterKey(otInstance *aInstance);
+char     *otThreadGetNetworkName(otInstance *aInstance);
 
 };
 
-
 void loop() {
 
-   int8_t myChannel = otLinkGetChannel(0);
-   uint16_t myPanID = otLinkGetPanId(0);    // otPanId
+        otInstance*  ot = ot_get_instance();
 
-   char myPanIDBuff[255];
-   snprintf(myPanIDBuff, sizeof(myPanIDBuff), "0x%X", myPanID);   // put hex into myPanIDBuff
+        int8_t myChannel = otLinkGetChannel(ot);
+        uint16_t myPanID = otLinkGetPanId(ot); // otPanId
 
-   uint8_t myMasterKey = otThreadGetMasterKey(0);
-   char *myNetworkName = otThreadGetNetworkName(0);
+        char myPanIDBuff[255];
+        snprintf(myPanIDBuff, sizeof(myPanIDBuff), "0x%02X", myPanID); // put hex into myPanIDBuff
 
-
-   char myNetworkNameBuff[255];
-   snprintf(myNetworkNameBuff, sizeof(myNetworkNameBuff), "%s", myNetworkName);   // network name as a string
+// Begin Changes
 
 
-   Particle.publish("----------------","-------------", 60, PRIVATE);    // just to show a space between samples
-   delay(2000);
-   Particle.publish("Particle Thread ","Connection Information", 60, PRIVATE);    // just to show a space between samples
-   delay(2000);
-   Particle.publish("My Channel: ", String(myChannel), 60, PRIVATE);  //shows printing an integer variable
+/*
 
-   delay(2000);
-   Particle.publish("Argon Pan ID: ", String(myPanIDBuff), 60, PRIVATE);  //shows printing an integer variable
-   delay(2000);
+   uint8_t myMasterKey = otThreadGetMasterKey(ot);
+   char myMasterKeyBuff[255];
+   snprintf(myMasterKeyBuff, sizeof(myMasterKeyBuff), "%s\0", &myMasterKey);
 
-   Particle.publish("My Master Key: ", String(myMasterKey), 60, PRIVATE);  //shows printing an integer variable
-   delay(2000);
-   Particle.publish("My Network Name: ", String(myNetworkNameBuff), 60, PRIVATE);  //shows printing an integer variable
-   delay(2000);
-   Particle.publish("----------------","-------------", 60, PRIVATE);    // just to show a space between samples
-   delay(2000);
-   Particle.publish("Generates the same info","every 60 seconds", 60, PRIVATE);    // just to show a space between samples
+ */
 
-   delay(60000);  // wait 60 seconds before re-print
+/*
+
+        uint8_t myMasterKeyPointer= otThreadGetMasterKey(ot);
+        const uint8_t *myMasterKey = &myMasterKeyPointer;
+        char myMasterKeyBuff[255];
+
+        // MasterKey is an array of 16 bytes (uint8_t)
+        // Convert the array to a string of 32 characters hex digits
+
+        for (unsigned i=0; i < sizeof(myMasterKey); i++) {
+                snprintf(myMasterKeyBuff, sizeof(myMasterKeyBuff), "%s%02X", myMasterKeyBuff, myMasterKey[i]);
+        }
+
+ */
+
+
+// Next Attempt
+
+        constexpr char hexmap[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+        const otMasterKey pKey = otThreadGetMasterKey(ot);
+        const otMasterKey *key = &pKey;
+        wchar_t szKey[OT_MASTER_KEY_SIZE * 2 + 1] = { 0 };
+        for (uint8_t i = 0; i < OT_MASTER_KEY_SIZE; i++)
+        {
+                szKey[2 * i]     = hexmap[(key->m8[i] & 0xF0) >> 4];
+                szKey[2 * i + 1] = hexmap[key->m8[i] & 0x0F];
+        }
+
+
+        char myMasterKeyBuff[255];
+        wcstombs(myMasterKeyBuff, szKey, sizeof(myMasterKeyBuff));
+
+// End changes
+
+        char *myNetworkName = otThreadGetNetworkName(ot);
+        char myNetworkNameBuff[255];
+        snprintf(myNetworkNameBuff, sizeof(myNetworkNameBuff), "%s", myNetworkName); // network name as a string
+
+
+        Particle.publish("----------------","-------------", 60, PRIVATE); // just to show a space between samples
+        delay(2000);
+        Particle.publish("Particle Thread ","Connection Information", 60, PRIVATE); // just to show a space between samples
+        delay(2000);
+        Particle.publish("My Channel: ", String(myChannel), 60, PRIVATE); //shows printing an integer variable
+
+        delay(2000);
+        Particle.publish("Argon Pan ID: ", String(myPanIDBuff), 60, PRIVATE); //shows printing an integer variable
+        delay(2000);
+
+        Particle.publish("My Master Key: ", String(myMasterKeyBuff), 60, PRIVATE); //shows printing an integer variable
+        delay(2000);
+        Particle.publish("My Network Name: ", String(myNetworkNameBuff), 60, PRIVATE); //shows printing an integer variable
+        delay(2000);
+        Particle.publish("----------------","-------------", 60, PRIVATE); // just to show a space between samples
+        delay(2000);
+        Particle.publish("Generates the same info","every 60 seconds", 60, PRIVATE); // just to show a space between samples
+
+        delay(60000); // wait 60 seconds before re-print
 }
